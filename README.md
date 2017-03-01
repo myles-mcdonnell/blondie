31fdcf08202fbcbf906a0061ae59633ba990a868
myles-mcdonnell-m3 <myles.mcdonnell@eu.m3.com>
2017-03-01 16:08:07 +0000

    enabled multiple targets

--- README.md
+++ README.md
@@ -1,20 +1,19 @@
 # Blondie
 
-A command that polls a TCP socket until a connection is made or the timeout is hit.  Useful if you need to wait for a server to start before doing something else.
+A command that polls one or more TCP sockets until a connection is made or the timeout is hit.  Useful if you need to wait for a server or group of servers to start before doing something else.
 
 Why call it Blondie? This is the best I could come up with in the 5 seconds I allowed for naming it [https://www.youtube.com/watch?v=uWhkbDMISl8](https://www.youtube.com/watch?v=uWhkbDMISl8)
 
 Example:
 
 ```
-//This will poll localhost:8080 every 500 milliseconds until a connection is made and it exits with code 0 and outputs 'Connection OK' or times out and exits code 2 with error message text output
-blondie -address=localhost:8080 -timeout=20 -poll-interval=500 -exit-code-on-connect=0 exit-code-on-fail=2 -quiet-mode=false
+//This will poll localhost:8080 and localhost:80 every 500 milliseconds until a connection is made and it exits with code 0 and outputs 'Connection OK' or the timeout is reached (20 and 5 seconds respectively) and exits code 2 with error message text output
+blondie -targets=localhost:8080:20,localhost:80:5 -poll-interval=500 -exit-code-on-connect=0 exit-code-on-fail=2 -quiet-mode=false
 ```
 
 Args:
 
-* timeout:  timeout in seconds, defaults to 5
-* address: host and port, e.g. localhost:8080
+* targets: comma separated host, port and timeoutSeconds, e.g. localhost:8080:20,localhost:80:5
 * poll-interval: poll interval in milliseconds, defaults to 250
 * exit-code-on-connect: defaults to 0
 * exit-code-on-fail: defaults to 1
--- blondie.go
+++ blondie.go
@@ -6,41 +6,78 @@ import (
 	"net"
 	"time"
 	"flag"
+	"strings"
+	"sync"
+	"strconv"
 )
+
+type outputWriter struct {
+	QuietMode bool
+}
 
 func main() {
 
-	timeout := flag.Float64("timeout", 5, "timeout in seconds")
-	address := flag.String("address", "", "address and port, e.g. localhost:8080")
+	targets := flag.String("targets", "", "comma separated address:port:timeoutSeconds, e.g. localhost:8080:60")
 	pollinterval := flag.Int("poll-interval", 250, "poll interval in milliseconds")
 	exitCodeOnConnectOk := flag.Int("exit-code-on-connect", 0, "Exit code when connection is made")
 	exitCodeOnConnectFail := flag.Int("exit-code-on-fail", 1, "Exit code when connection is not made")
-	quietMode := flag.Bool("quiet-mode", true, "suppress all output")
+	quietMode := flag.Bool("quiet-mode", false, "suppress all output")
 	flag.Parse()
 
-	if *address == "" {
-		fmt.Println("Please supply address, e.g. -address=localhost:8080")
-		os.Exit(*exitCodeOnConnectFail)
-	}
+	var waitGroup = &sync.WaitGroup{}
+	var writer = &outputWriter{QuietMode:*quietMode}
+	var failure bool
 
-	start := time.Now()
+	for _, target := range strings.Split(*targets, ",") {
+		addrPortAndTimeout := strings.Split(target, ":")
 
-	for true {
+		var address = addrPortAndTimeout[0]+":"+addrPortAndTimeout[1]
 
-		_, err := net.Dial("tcp", *address)
-
-		if (err==nil) {
-			if !*quietMode {
-				fmt.Println("Connection OK")
-			}
-			os.Exit(*exitCodeOnConnectOk)
-		} else if (time.Now().Sub(start).Seconds()>*timeout) {
-			if !*quietMode {
-				fmt.Println(err.Error())
-			}
+		if address == "" {
+			fmt.Println("Please supply targets, e.g. -targets=localhost:8080;60,google.com:80;30")
 			os.Exit(*exitCodeOnConnectFail)
 		}
 
-		time.Sleep(time.Millisecond*time.Duration(*pollinterval))
+		var timeout, err = strconv.Atoi(addrPortAndTimeout[2])
+		if err!=nil {
+			panic(err)
+		}
+		start := time.Now()
+
+		waitGroup.Add(1)
+
+		writer.Write(fmt.Sprintf("Trying to connect: %s", address))
+		go func(){
+			for true {
+				_, err := net.Dial("tcp", address)
+
+				if (err==nil) {
+					writer.Write(fmt.Sprintf("Connection OK : %s", address))
+					waitGroup.Add(-1)
+					break//os.Exit(*exitCodeOnConnectOk)
+				} else if (time.Now().Sub(start).Seconds()>float64(timeout)) {
+					writer.Write(fmt.Sprintf("%s : %s", err.Error(), address))
+					waitGroup.Add(-1)
+					failure = true
+					break//os.Exit(*exitCodeOnConnectFail)
+				}
+
+				time.Sleep(time.Millisecond*time.Duration(*pollinterval))
+			}
+		}()
+	}
+
+	waitGroup.Wait()
+
+	if failure {
+		os.Exit(*exitCodeOnConnectFail)
+	}
+
+	os.Exit(*exitCodeOnConnectOk)
+}
+
+func (writer outputWriter) Write(message string) {
+	if !writer.QuietMode {
+		fmt.Println(message)
 	}
 }
